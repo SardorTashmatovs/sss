b5ee3013b2ee02cd3c35315e4539dea9a3b882cc
SardorTashmatovs <155821096+SardorTashmatovs@users.noreply.github.com>
2024-02-13 17:20:50 +0500

    Add files via upload

---
+++ p.py
@@ -0,0 +1,51 @@
+import requests
+
+#  метод Put
+# params = {'id': '1', 'title': 'asss'}
+# aa = requests.put('https://jsonplaceholder.typicode.com/posts/1', params=params)
+# print(aa.status_code)
+
+
+#  метод Patch
+# params = {'title': 'asss', 'body': 'asss', 'userId': '1'}
+# ad = requests.patch('https://jsonplaceholder.typicode.com/posts/1', params=params)
+# print(ad.status_code)
+
+# метод DELETE
+# params = {'id': '1', 'aaaa':'aaaasdd'}
+
+# adff = requests.delete('https://jsonplaceholder.typicode.com/posts/1', params=params)
+# print(adff.status_code)
+
+
+
+
+
+
+# 3
+
+
+
+# 1
+# params = {'id': '1', 'title': 'asss'}
+# aa = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json', params=params)
+# print(aa.status_code)
+
+# 2
+# params = {'id': '1', 'title': 'asss'}
+# aa = requests.get('https://api.genderize.io/?name=vadim', params=params)
+# print(aa.status_code)
+
+
+
+# 3
+# params = {'id': '1', 'title': 'asss'}
+# aa = requests.get('https://ipinfo.io/161.185.160.93/geo', params=params)
+# print(aa.status_code)
+
+
+# 4
+
+# params = {'id': '1', 'title': 'asss'}
+# aa = requests.get('https://randomuser.me/api/', params=params)
+# print(aa.status_code)
